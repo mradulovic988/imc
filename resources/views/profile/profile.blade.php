6fbe9d3ffca075b6275c17af356e981eff94216f
markopopart <upss070288@gmail.com>
2022-10-05 10:09:01 +0200

    Added all archive and single pages

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -121,7 +121,7 @@
                             </h2>
 
                             <div class="mt-8">
-                                <div class="bg-red-50">
+                                <div class="bg-red-50 sm:rounded-lg">
                                     <div class="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
                                         <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
                                             <span class="block text-red-600">Delete your account?</span>
